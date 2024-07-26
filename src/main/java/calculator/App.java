925a69180135afbe8e4739bd09502d4cdcd30d5d
Aakaive <wonseok031@gmail.com>
2024-07-26 11:44:16 +0900

    level 1 - requirement 05

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,6 +7,8 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int result = 0;
+        int[] intArr = new int[10];
+        int count = 0;
 
         while(true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -24,21 +26,40 @@ public class App {
                         break;
                     }
                     result = a / b;
+                    if(count < 10) {
+                        intArr[count] = result;
+                        count++;
+                    }
                     break;
                 case '+':
                     result = a + b;
+                    if(count < 10) {
+                        intArr[count] = result;
+                        count++;
+                    }
                     break;
                 case '-':
                     result = a - b;
+                    if(count < 10) {
+                        intArr[count] = result;
+                        count++;
+                    }
                     break;
                 case '*':
                     result = a * b;
+                    if(count < 10) {
+                        intArr[count] = result;
+                        count++;
+                    }
                     break;
                 default:
                     break;
             }
 
-            System.out.println("결과: " + result);
+            System.out.println("결과 : " + result);
+//            for(int i : intArr) {
+//                System.out.println("지난 결과 : " + i);
+//            }
 
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
             String str = sc.nextLine();
