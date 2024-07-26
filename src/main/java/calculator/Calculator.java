fd4e7dee6ad0cb35dca9ab1a0af0192b6d4f931a
Aakaive <wonseok031@gmail.com>
2024-07-26 12:48:47 +0900

    level 2 - requirement 04

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -15,7 +15,6 @@ public class Calculator {
                     result = dt.devideTest(a, b);
                 } catch (ArithmeticException e){
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    return 0;
                 }
 
                 break;
