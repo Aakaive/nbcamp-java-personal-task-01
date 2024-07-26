6f97c054a54128fa166cbe882eef36d06c19ec70
Aakaive <wonseok031@gmail.com>
2024-07-26 10:45:31 +0900

    test this project

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,6 +10,7 @@ public class App {
             String str = sc.nextLine();
             if(str.equals("exit")) break;
             else if(str.equals("asdf")) System.out.println("그거 아닌데");
+            else System.out.println("존재하지 않는 명령어");
         }
     }
 }
