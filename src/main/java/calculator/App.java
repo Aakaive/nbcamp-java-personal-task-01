a70da7cf6924584e5038d4d8be73d9b33bc5c4c2
Aakaive <wonseok031@gmail.com>
2024-07-26 11:35:03 +0900

    level 1 - requirement 03

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,7 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int result = 0;
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int a = sc.nextInt();
@@ -13,5 +14,28 @@ public class App {
         int b = sc.nextInt();
         System.out.print("사칙연산 기호를 입력하세요: ");
         char c = sc.next().charAt(0);
+
+        switch (c) {
+            case '/' :
+                if(b == 0){
+                    System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = a / b;
+                break;
+            case '+' :
+                result = a + b;
+                break;
+            case '-' :
+                result = a - b;
+                break;
+            case '*' :
+                result = a * b;
+                break;
+            default :
+                break;
+        }
+
+        System.out.println("결과: " + result);
     }
 }
