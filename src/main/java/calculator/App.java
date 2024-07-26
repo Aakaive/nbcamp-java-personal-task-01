2153f3a228e660070936bb339cafa00a0bdb62a5
Aakaive <wonseok031@gmail.com>
2024-07-26 11:27:20 +0900

    level 1 - requirement 02

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,9 +7,11 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("첫 번째 숫자를 입력하세요: ");
+        System.out.print("첫 번째 숫자를 입력하세요: ");
         int a = sc.nextInt();
-        System.out.println("두 번째 숫자를 입력하세요: ");
+        System.out.print("두 번째 숫자를 입력하세요: ");
         int b = sc.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char c = sc.next().charAt(0);
     }
 }
