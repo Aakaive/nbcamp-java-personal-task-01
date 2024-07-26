01f226f5946f31fe31ccaf580dbaa38eb56725e8
Aakaive <wonseok031@gmail.com>
2024-07-26 11:48:01 +0900

    level 1 - requirement 05

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,11 +6,12 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int result = 0;
         int[] intArr = new int[10];
         int count = 0;
 
         while(true) {
+            int result = 0;
+
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int a = sc.nextInt();
             System.out.print("두 번째 숫자를 입력하세요: ");
@@ -26,40 +27,29 @@ public class App {
                         break;
                     }
                     result = a / b;
-                    if(count < 10) {
-                        intArr[count] = result;
-                        count++;
-                    }
                     break;
                 case '+':
                     result = a + b;
-                    if(count < 10) {
-                        intArr[count] = result;
-                        count++;
-                    }
                     break;
                 case '-':
                     result = a - b;
-                    if(count < 10) {
-                        intArr[count] = result;
-                        count++;
-                    }
                     break;
                 case '*':
                     result = a * b;
-                    if(count < 10) {
-                        intArr[count] = result;
-                        count++;
-                    }
                     break;
                 default:
                     break;
             }
 
+            if(count < 10) {
+                intArr[count] = result;
+                count++;
+            }
+
             System.out.println("결과 : " + result);
-//            for(int i : intArr) {
-//                System.out.println("지난 결과 : " + i);
-//            }
+            for(int i : intArr) {
+                System.out.println("지난 결과 : " + i);
+            }
 
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
             String str = sc.nextLine();
