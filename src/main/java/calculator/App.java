aea178e2ef0d350122ce95300291fbb8714e6bb2
Aakaive <wonseok031@gmail.com>
2024-07-26 10:39:54 +0900

    text this project

---
+++ src/main/java/calculator/App.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String str = sc.nextLine();
+            if(str.equals("exit")) break;
+            else if(str.equals("asdf")) System.out.println("그거 아닌데");
+        }
+    }
+}
