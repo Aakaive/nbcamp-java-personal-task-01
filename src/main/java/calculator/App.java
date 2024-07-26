3ce60bd707075ed54a732b5ad9fbe6521e8584cb
Aakaive <wonseok031@gmail.com>
2024-07-26 11:40:35 +0900

    level 1 - requirement 04

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,34 +8,42 @@ public class App {
         Scanner sc = new Scanner(System.in);
         int result = 0;
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int a = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int b = sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char c = sc.next().charAt(0);
+        while(true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int a = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int b = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char c = sc.next().charAt(0);
+            sc.nextLine();
 
-        switch (c) {
-            case '/' :
-                if(b == 0){
-                    System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+            switch (c) {
+                case '/':
+                    if (b == 0) {
+                        System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                        break;
+                    }
+                    result = a / b;
                     break;
-                }
-                result = a / b;
-                break;
-            case '+' :
-                result = a + b;
-                break;
-            case '-' :
-                result = a - b;
-                break;
-            case '*' :
-                result = a * b;
-                break;
-            default :
-                break;
-        }
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                default:
+                    break;
+            }
 
-        System.out.println("결과: " + result);
+            System.out.println("결과: " + result);
+
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
+            String str = sc.nextLine();
+
+            if(str.equals("exit")) break;
+        }
     }
 }
